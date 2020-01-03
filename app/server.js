6bf99644a88ffa3e4ce194302673c6c54a9a47c3
stevenktruong <steventruong@g.ucla.edu>
2020-01-02 22:18:50 -0800

    Added basic logging to the server

--- app/server.js
+++ app/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
 
+app.use(morgan("tiny"));
 app.use(express.static(`${process.cwd()}/public`));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
