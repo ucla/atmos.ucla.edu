678bb4ede8a8e97de14ba720ad824d33de4016d0
stevenktruong <steventruong@g.ucla.edu>
2020-01-02 21:46:27 -0800

    The website now gets served

--- app/server.js
+++ app/server.js
@@ -3,7 +3,6 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.use("/", express.static(`${process.cwd()}/public/index.html`));
-console.log(`${process.cwd()}/public/index.html`);
+app.use(express.static(`${process.cwd()}/public`));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
