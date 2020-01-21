6f3f5dbf0498d091b1e8b934698beb0c399b8bf2
stevenktruong <steventruong@g.ucla.edu>
2020-01-21 13:55:24 -0800

    Temporary changes to authentication

--- app/server.js
+++ app/server.js
@@ -1,10 +1,36 @@
+import http from "http";
 import express from "express";
 import morgan from "morgan";
+import dotenv from "dotenv";
+import passport from "passport";
+// import { OAuthStrategy } from "passport-google-oauth";
+
+// Add environment variables from .env file to process.env variable
+dotenv.config();
+
+// const GoogleStrategy = OAuthStrategy.GoogleStrategy;
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       consumerKey: GOOGLE_CONSUMER_KEY,
+//       consumerSecret: GOOGLE_CONSUMER_SECRET,
+//       callbackURL: "http://www.example.com/auth/google/callback"
+//     },
+//     (token, tokenSecret, profile, done) => {
+//       User.findOrCreate({ googleId: profile.id }, function(err, user) {
+//         return done(err, user);
+//       });
+//     }
+//   )
+// );
 
 const app = express();
-const port = 3000;
 
+// Log requests
 app.use(morgan("tiny"));
+
+// Serve public pages
 app.use(express.static(`${process.cwd()}/public`));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// Start server
+http.createServer(app).listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
