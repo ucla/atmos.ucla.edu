ca3bce90ff3779f6361b7e98a4c16d4d26b63d75
stevenktruong <steventruong@g.ucla.edu>
2020-01-22 13:16:26 -0800

    Attempt at Google auth

--- app/server.js
+++ app/server.js
@@ -3,31 +3,34 @@ import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import passport from "passport";
-// import { OAuthStrategy } from "passport-google-oauth";
+import { OAuth2Strategy as GoogleStrategy } from "passport-google-oauth";
 
 // Add environment variables from .env file to process.env variable
 dotenv.config();
 
-// const GoogleStrategy = OAuthStrategy.GoogleStrategy;
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       consumerKey: GOOGLE_CONSUMER_KEY,
-//       consumerSecret: GOOGLE_CONSUMER_SECRET,
-//       callbackURL: "http://www.example.com/auth/google/callback"
-//     },
-//     (token, tokenSecret, profile, done) => {
-//       User.findOrCreate({ googleId: profile.id }, function(err, user) {
-//         return done(err, user);
-//       });
-//     }
-//   )
-// );
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: "http://cms.atmos.ucla.edu:3000/admin"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+      User.findOrCreate({ googleId: profile.id }, (err, user) => {
+        return done(err, user);
+      });
+    }
+  )
+);
 
 const app = express();
 
 // Log requests
 app.use(morgan("tiny"));
+
+// Serve admin page
+app.get("/auth", passport.authenticate("google", { scope: ["https://www.googleapis.com/auth/plus.login"] }));
 
 // Serve public pages
 app.use(express.static(`${process.cwd()}/public`));
